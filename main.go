baa2b9685047a8b712d0ccc10ab4719f706433e6
Jul Kreshpaj <jul@paperclicks.net>
2019-01-07 21:31:33 +0100

    added logo

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/flakaal/flaka-ci/cmd"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type ServerConfig struct {
+	Dir      string
+	Services map[string]map[string]interface{} `yaml:"services"`
+	Port     int                               `yaml:"port"`
+}
 
 func main() {
+	c := ServerConfig{}
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadFile(dir + "/flaka-ci.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := yaml.Unmarshal([]byte(data), &c); err != nil {
+		log.Fatal(err)
+	}
 
-	cmd.Execute()
+	// arr := reflect.ValueOf(c.Services["test"]["command"])
+	// for i := 0; i < arr.Len(); i++ {
+	// 	fmt.Printf(arr.Index(i).Elem().String())
+	// }
 
+	// cmd.Execute()
 }
