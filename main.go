f5c575854d27b56786a04115e72c87be92204449
Jul Kreshpaj <julikreshpaj@gmail.com>
2019-01-10 10:28:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,5 @@ package main
 import "github.com/flakaal/flaka-ci/cmd"
 
 func main() {
-	//slack webhook https://hooks.slack.com/services/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
 	cmd.Execute()
 }
